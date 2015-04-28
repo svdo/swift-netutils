b9672d7f12d012e960c23d9779a39002699b43d1
Stefan van den Oord <soord@mac.com>
2015-04-28 20:53:50 +0200

    Initial Commit

---
+++ NetUtils/NetUtils.h
@@ -0,0 +1,19 @@
+//
+//  NetUtils.h
+//  NetUtils
+//
+//  Created by Stefan on 28/04/15.
+//  Copyright (c) 2015 Stefan van den Oord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetUtils.
+FOUNDATION_EXPORT double NetUtilsVersionNumber;
+
+//! Project version string for NetUtils.
+FOUNDATION_EXPORT const unsigned char NetUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetUtils/PublicHeader.h>
+
+
