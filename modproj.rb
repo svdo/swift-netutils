3ba3c0eb57006928e0754b8691d9722745b244ba
Stefan van den Oord <soord@mac.com>
2015-05-03 13:08:17 +0200

    Gonna try a simpler method based on xcconfig in podspec

--- modproj.rb
+++
@@ -1,6 +0,0 @@
-require 'xcodeproj'
-project = Xcodeproj::Project.open('NetUtils.xcodeproj')
-main_target = project.targets.first
-phase = main_target.new_shell_script_build_phase("Create ifaddrs module map")
-phase.shell_script = "${SRCROOT}/createModuleMap.sh"
-project.save()
