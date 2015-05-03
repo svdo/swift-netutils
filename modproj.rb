0d38bcad3c6fdf9fb77006178d63c4aa73bbd0e4
Stefan van den Oord <soord@mac.com>
2015-05-03 07:55:24 +0200

    ...

--- modproj.rb
+++ modproj.rb
@@ -1,7 +1,6 @@
 require 'xcodeproj'
-puts ENV['PROJECT_FILE_PATH']
-project = Xcodeproj::Project.open(ENV['PROJECT_FILE_PATH'])
+project = Xcodeproj::Project.open('NetUtils.xcodeproj')
 main_target = project.targets.first
 phase = main_target.new_shell_script_build_phase("Create ifaddrs module map")
-phase.shell_script = "do sth with ${SRCROOT}/createModuleMap.sh"
+phase.shell_script = "${SRCROOT}/createModuleMap.sh"
 project.save()
