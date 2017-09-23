cbea20a89342957e1467ba2cea77ee8551c5c84a
Stefan van den Oord <soord@mac.com>
2017-09-23 10:12:18 +0200

    Test app for Xocde 8 with Swift 3

---
+++ Tests/Xcode8Swift3/App/App/ViewController.swift
@@ -0,0 +1,12 @@
+//  Copyright © 2017 Stefan van den Oord. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
