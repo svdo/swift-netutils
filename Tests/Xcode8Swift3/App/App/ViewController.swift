34269418ced5907aeb564eb49b30f3d69f84381f
Stefan van den Oord <soord@mac.com>
2017-09-23 10:38:48 +0200

    Add call to NetUtils to make sure it compiles correctly

--- Tests/Xcode8Swift3/App/App/ViewController.swift
+++ Tests/Xcode8Swift3/App/App/ViewController.swift
@@ -1,11 +1,13 @@
 //  Copyright © 2017 Stefan van den Oord. All rights reserved.
 
 import UIKit
+import NetUtils
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("\(Interface.allInterfaces())")
     }
 
 }
