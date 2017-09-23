860edf1f06553fee60eb6880f5705d566c506659
Stefan van den Oord <soord@mac.com>
2017-09-23 13:11:05 +0200

    Remove the ifaddrs module maps if Xcode 9 or later is used; they are not needed anymore.

--- ifaddrs/injectXcodePath.sh
+++ ifaddrs/injectXcodePath.sh
@@ -26,10 +26,19 @@ scriptName="`basename $0`"
 absScriptDir="`cd $scriptDir; pwd`"
 
 main() {
-    for f in `find ${absScriptDir} -name module.modulemap`; do
-        cat ${f} | sed "s,${defaultXcodePath},${realXcodePath},g" > ${f}.new || fatal "Failed to update modulemap ${f}"
-        mv ${f}.new ${f} || fatal "Failed to replace modulemap ${f}"
-    done
+    xcodeMajor=$(xcrun xcodebuild -version|grep -E "Xcode \d+\.\d+(\.\d+)?"|cut -d ' ' -f 2|cut -d '.' -f 1)
+    set -i xcodeMajor
+    if [ ${xcodeMajor} -lt 9 ]; then
+        echo "Xcode prior to 9: ifaddrs module maps needed."
+        for f in `find ${absScriptDir} -name module.modulemap`; do
+            cat ${f} | sed "s,${defaultXcodePath},${realXcodePath},g" > ${f}.new || fatal "Failed to update modulemap ${f}"
+            mv ${f}.new ${f} || fatal "Failed to replace modulemap ${f}"
+        done
+    else
+        echo "Xcode 9 and above: ifaddrs module maps not needed."
+        rm */module.modulemap
+    fi
+
 }
 
 main $*
