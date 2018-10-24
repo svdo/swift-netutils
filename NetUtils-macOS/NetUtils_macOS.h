8412fa57f691cd4d3e78c22146ba131744a507fd
Stefan van den Oord <soord@mac.com>
2018-10-24 21:11:02 +0200

    Removes macOS library target (should be framework)

--- NetUtils-macOS/NetUtils_macOS.h
+++ NetUtils-macOS/NetUtils_macOS.h
@@ -1,7 +1,19 @@
+//
+//  NetUtils_macOS.h
+//  NetUtils-macOS
+//
+//  Created by Stefan on 24/10/2018.
 //  Copyright © 2018 Stefan van den Oord. All rights reserved.
+//
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
-@interface NetUtils_macOS : NSObject
+//! Project version number for NetUtils_macOS.
+FOUNDATION_EXPORT double NetUtils_macOSVersionNumber;
 
-@end
+//! Project version string for NetUtils_macOS.
+FOUNDATION_EXPORT const unsigned char NetUtils_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetUtils_macOS/PublicHeader.h>
+
+
--- NetUtils-macOS/NetUtils_macOS.m
+++
@@ -1,7 +0,0 @@
-//  Copyright © 2018 Stefan van den Oord. All rights reserved.
-
-#import "NetUtils_macOS.h"
-
-@implementation NetUtils_macOS
-
-@end
