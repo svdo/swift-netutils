82f960895e87c0500bc97cfb7054b7482c01018a
Stefan van den Oord <soord@mac.com>
2018-11-11 19:46:21 +0100

    Switches to Xcode 10.1 for Travis testing

--- Tests/Swift4/runTest.sh
+++ Tests/Swift4/runTest.sh
@@ -6,8 +6,8 @@ scriptdir=$(cd $(dirname $0) && pwd)
 echo "Running Pod test for $(basename ${scriptdir})."
 
 xcodeVersion=$(xcrun xcodebuild -version)
-if ( ! ( echo ${xcodeVersion} | grep "Xcode 9" > /dev/null 2>&1 ) ); then
-    echo "Expected Xcode 9.x, got "$(xcrun xcodebuild -version|grep "Xcode ")
+if ( ! ( echo ${xcodeVersion} | grep "Xcode 10" > /dev/null 2>&1 ) ); then
+    echo "Expected Xcode 10.x, got "$(xcrun xcodebuild -version|grep "Xcode ")
     exit 1
 fi
 
