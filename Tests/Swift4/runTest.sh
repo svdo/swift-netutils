28eb6f86a84e34e97dd3bea7e9658da15e8a1767
Stefan van den Oord <soord@mac.com>
2022-07-25 21:01:16 +0200

    Basing test on Xcode 13 now

--- Tests/Swift4/runTest.sh
+++ Tests/Swift4/runTest.sh
@@ -6,8 +6,8 @@ scriptdir=$(cd $(dirname $0) && pwd)
 echo "Running Pod test for $(basename ${scriptdir})."
 
 xcodeVersion=$(xcrun xcodebuild -version)
-if ( ! ( echo ${xcodeVersion} | grep "Xcode 12" > /dev/null 2>&1 ) ); then
-    echo "Expected Xcode 12.x, got "$(xcrun xcodebuild -version|grep "Xcode ")
+if ( ! ( echo ${xcodeVersion} | grep "Xcode 13" > /dev/null 2>&1 ) ); then
+    echo "Expected Xcode 13.x, got "$(xcrun xcodebuild -version|grep "Xcode ")
     exit 1
 fi
 
