c34d0563354f5997fd64a511a768402a40f0c927
Stefan van den Oord <soord@mac.com>
2022-07-25 21:45:05 +0200

    Add test for Swift5

---
+++ Tests/Swift5/runTest.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -o pipefail
+
+scriptdir=$(cd $(dirname $0) && pwd)
+echo "Running Pod test for $(basename ${scriptdir})."
+
+xcodeVersion=$(xcrun xcodebuild -version)
+if ( ! ( echo ${xcodeVersion} | grep "Xcode 13" > /dev/null 2>&1 ) ); then
+    echo "Expected Xcode 13.x, got "$(xcrun xcodebuild -version|grep "Xcode ")
+    exit 1
+fi
+
+(
+    cd $(dirname $0)/App
+    pod install
+    xcrun xcodebuild -workspace App.xcworkspace -scheme App -sdk iphonesimulator | xcpretty
+)
